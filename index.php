798bbcd695a33c64a2e394d6eeed367d0f898b05
St4mPzSeVeN <jiwjirawat@gmail.com>
2020-03-22 19:09:44 +0700

    V 1.3
    V1.3

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
 &nbsp;
 </div>&nbsp;
 	<div class="container justify-content-center" style="
-	background-image: url('img/Surface-1.png');
+	background-image: url('img/Surface-2.png');
 	background-repeat: no-repeat;
 	background-position: center;
 	background-size: auto;
@@ -77,10 +77,10 @@
 	onclick="location.href='surface.php';" 
 	tabIndex="0"
 	>
-	<h1 style="text-align: center;">
+	<h1 style="text-align: center; color: #ff6600;">
 	SURFACE PRO X
 	</h1><br>
-	<h5 style="text-align: center;">
+	<h5 style="text-align: center; color: #ffa366;">
 	A NEW GENERATION OF SURFACE.
 	</h5>
 	</div>
@@ -100,10 +100,10 @@
 	onclick="location.href='swift.php';" 
 	tabIndex="0"
 	>
-	<h1 style="text-align: center;">
+	<h1 style="text-align: center; color: #33cc33;">
 	SWIFT 7
 	</h1><br>
-	<h5 style="text-align: center;">
+	<h5 style="text-align: center; color: #85e085;">
 	LIGHT AND POWERFUL.
 	</h5>
 	</div>
@@ -116,16 +116,16 @@
 	background-repeat: no-repeat;
 	background-position: center;
 	background-size: auto;
-	padding-top: 3vh;
-	padding-bottom: 67vh;
+	padding-top: 25vh;
+	padding-bottom: 45vh;
 	"
 	onclick="location.href='xps.php';" 
 	tabIndex="0"
 	>
-	<h1 style="text-align: center;">
+	<h1 style="text-align: center; color: #ffaa00;">
 	XPS
 	</h1><br>
-	<h5 style="text-align: center;">
+	<h5 style="text-align: center; color: #ffcc66;">
 	WORLD OF EDGE TO EDGE DISPLAY.
 	</h5>
 	</div>
@@ -144,15 +144,15 @@
 	onclick="location.href='zenbook.php';" 
 	tabIndex="0"
 	>
-	<h1 style="text-align: center;">
+	<h1 style="text-align: center; color: #ff00bf;">
 	ZENBOOK PRO
 	</h1><br>
-	<h5 style="text-align: center;">
+	<h5 style="text-align: center; color: #ff66d9;">
 	EXTEND THE IMAGINATION.
 	</h5>
 	</div>
 	&nbsp;
-<footer style="background-color: #262626"><br><br></footer>
+<footer style="background-color: #262626; text-align: center;"><br> <p1 style="color: white; text-align: center; font-family: Kanit; font-size: 1.5vh;">The content, information, opinions, and viewpoints contained in these educational materials are those of the authors or contributors of such materials. While the American Society of Transplantation (AST) and its committees take great care to screen the credentials of the contributors and make every attempt to review the contents, AST MAKES NO WARRANTY, EXPRESSED OR IMPLIED, as to the completeness or accuracy of the content contained in the educational materials or on this website. The reader of these materials uses these materials at his or her own risk, and AST shall not be responsible for any errors, omissions, or inaccuracies in these materials, whether arising through negligence, oversight, or otherwise. Reliance on any information appearing on this site is strictly at your own risk.</p1> <br><br></footer>
 </div>
 </body>
 </html>
--- swift.php
+++ swift.php
@@ -100,7 +100,7 @@
 </div>
 <br><br>
 &nbsp;
-<footer style="background-color: #262626"><br><br></footer>
+<footer style="background-color: #262626; text-align: center;"><br> <p1 style="color: white; text-align: center; font-family: Kanit; font-size: 1.5vh;">The content, information, opinions, and viewpoints contained in these educational materials are those of the authors or contributors of such materials. While the American Society of Transplantation (AST) and its committees take great care to screen the credentials of the contributors and make every attempt to review the contents, AST MAKES NO WARRANTY, EXPRESSED OR IMPLIED, as to the completeness or accuracy of the content contained in the educational materials or on this website. The reader of these materials uses these materials at his or her own risk, and AST shall not be responsible for any errors, omissions, or inaccuracies in these materials, whether arising through negligence, oversight, or otherwise. Reliance on any information appearing on this site is strictly at your own risk.</p1> <br><br></footer>
 </div>
 </body>
 </html>
